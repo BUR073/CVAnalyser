8c3869670eb33d118172908b7e14f8c9ed420a8e
BUR073 <burbridgeh1@gmail.com>
2025-02-27 12:53:01 +0000

    Added vars to records

--- src/main/java/com/trackgenesis/records/CV.java
+++ src/main/java/com/trackgenesis/records/CV.java
@@ -1,4 +1,11 @@
 package com.trackgenesis.records;
 
-public record CV() {
-}
+public record CV(
+        String education,
+        String experience,
+        String skills,
+        String name,
+        String email,
+        String jobTitles,
+        int phoneNumber
+) {}
--- src/main/java/com/trackgenesis/records/JobDescription.java
+++ src/main/java/com/trackgenesis/records/JobDescription.java
@@ -1,4 +1,9 @@
 package com.trackgenesis.records;
 
-public record JobDescription() {
-}
+public record JobDescription(
+        String educationRequirements,
+        String skills,
+        String jobTitle,
+        String responsibilities,
+        String qualifications
+) {}
