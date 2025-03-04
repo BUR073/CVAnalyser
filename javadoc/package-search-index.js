46536a4e1fca5d0f83ad6b736efafd2079dcf9e6
BUR073 <burbridgeh1@gmail.com>
2025-03-04 19:21:44 +0000

    removed javadoc conents to reduce file size

--- javadoc/package-search-index.js
+++
@@ -1,5 +0,0 @@
-packageSearchIndex = [{
-    "l": "All Packages",
-    "u": "allpackages-index.html"
-}, {"l": "com.trackgenesis"}, {"l": "com.trackgenesis.auth"}, {"l": "com.trackgenesis.UI"}, {"l": "com.trackgenesis.util"}];
-updateSearchResults();
