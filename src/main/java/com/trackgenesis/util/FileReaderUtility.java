51542ef9cfaac58be7fccf5dfd5b9e966e6b35c6
BUR073 <burbridgeh1@gmail.com>
2025-03-19 23:48:45 +0000

    imporoved error handling

--- src/main/java/com/trackgenesis/util/FileReaderUtility.java
+++ src/main/java/com/trackgenesis/util/FileReaderUtility.java
@@ -11,62 +11,64 @@ public class FileReaderUtility {
 
     public String getText(String filePath) {
         String fileExtension = getFileExtension(filePath).toLowerCase();
-        StringBuilder content = new StringBuilder();
+        StringBuilder content;
 
-        try {
-            content = switch (fileExtension) {
-                case "txt" -> readTextFile(filePath);
-                case "pdf" -> readPdfFile(filePath);
-                case "docx" -> readDocxFile(filePath);
-                case "doc" -> readDocFile(filePath);
-                default -> throw new UnsupportedOperationException("File type not supported");
-            };
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-            return "";
-        } catch (UnsupportedOperationException e) {
-            System.err.println(e.getMessage());
-            return "";
-        }
+
+        content = switch (fileExtension) {
+            case "txt" -> readTextFile(filePath);
+            case "pdf" -> readPdfFile(filePath);
+            case "docx" -> readDocxFile(filePath);
+            case "doc" -> readDocFile(filePath);
+            default -> throw new IllegalStateException("Unexpected value: " + fileExtension);
+
+        };
 
         return content.toString();
     }
 
-    private StringBuilder readTextFile(String filePath) throws IOException {
+    private StringBuilder readTextFile(String filePath) {
         StringBuilder content = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 content.append(line).append(System.lineSeparator());
             }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
         return content;
     }
 
-    private StringBuilder readPdfFile(String filePath) throws IOException {
+    private StringBuilder readPdfFile(String filePath) {
         StringBuilder content = new StringBuilder();
         try (PDDocument document = PDDocument.load(new File(filePath))) {
             PDFTextStripper stripper = new PDFTextStripper();
             content.append(stripper.getText(document));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
         return content;
     }
 
-    private StringBuilder readDocxFile(String filePath) throws IOException {
+    private StringBuilder readDocxFile(String filePath) {
         StringBuilder content = new StringBuilder();
         try (InputStream fis = new FileInputStream(filePath)) {
             XWPFDocument document = new XWPFDocument(fis);
             document.getParagraphs().forEach(paragraph -> content.append(paragraph.getText()).append(System.lineSeparator()));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
         return content;
     }
 
-    private StringBuilder readDocFile(String filePath) throws IOException {
+    private StringBuilder readDocFile(String filePath) {
         StringBuilder content = new StringBuilder();
         try (InputStream fis = new FileInputStream(filePath)) {
             HWPFDocument document = new HWPFDocument(fis);
             Range range = document.getRange();
             content.append(range.text());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
         return content;
     }
