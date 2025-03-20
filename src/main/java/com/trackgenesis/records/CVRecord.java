e14e3ea10b15a35a34505db304ec52d0b4887f42
BUR073 <burbridgeh1@gmail.com>
2025-03-20 14:41:12 +0000

    Javadoc for records package

--- src/main/java/com/trackgenesis/records/CVRecord.java
+++ src/main/java/com/trackgenesis/records/CVRecord.java
@@ -4,6 +4,17 @@ package com.trackgenesis.records;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Record to store data parsed from CVs
+ * @param fileName the name of the file
+ * @param people Set of people
+ * @param organizations Set of organizations
+ * @param dates Set of dates
+ * @param times Set of times
+ * @param skills Set of skills
+ * @param email Email address
+ * @param phoneNumber Phone number
+ */
 public record CVRecord(
         String fileName,
         Set<String> people,
@@ -15,6 +26,10 @@ public record CVRecord(
         String phoneNumber
 ) {
 
+    /**
+     * Constructor
+     * Initialise initial values, mostly HashSets
+     */
     public CVRecord() {
         this("", new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), null, null);
     }
--- src/main/java/com/trackgenesis/records/JobDescriptionRecord.java
+++ src/main/java/com/trackgenesis/records/JobDescriptionRecord.java
@@ -4,6 +4,16 @@ package com.trackgenesis.records;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Record to store data parsed from job description
+ * @author henryburbridge
+ *
+ * @param locations location data
+ * @param organizations Set of organizations
+ * @param dates Set of dates
+ * @param times Set of times
+ * @param skills Set of skills
+ */
 public record JobDescriptionRecord(
         Set<String> locations,
         Set<String> organizations,
@@ -11,6 +21,10 @@ public record JobDescriptionRecord(
         Set<String> times,
         Set<String> skills
 ) {
+    /**
+     * Constructor
+     * Initializes HashSets for each variable in record
+     */
     public JobDescriptionRecord() {
         this(new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>());
     }
