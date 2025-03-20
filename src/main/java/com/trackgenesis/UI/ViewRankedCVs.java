2e0e8ba3afccda1f5605dcfa9529e81c70603958
BUR073 <burbridgeh1@gmail.com>
2025-03-20 14:02:59 +0000

    format and order the table for view ranked CVs

--- src/main/java/com/trackgenesis/UI/ViewRankedCVs.java
+++ src/main/java/com/trackgenesis/UI/ViewRankedCVs.java
@@ -6,7 +6,10 @@ import com.trackgenesis.records.JobDescriptionRecord;
 import com.trackgenesis.records.RecordRepository;
 import org.apache.commons.math3.util.Pair;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
 
 public class ViewRankedCVs {
     private final RecordRepository recordRepo;
@@ -17,22 +20,47 @@ public class ViewRankedCVs {
 
     }
 
-
-    public void view(){
+    public void view() {
         JobDescriptionRecord jdRecord = this.recordRepo.getJobDescriptionRecord();
         List<CVRecord> cvRecords = this.recordRepo.getCVRecord();
 
         if (jdRecord != null && cvRecords != null) {
-            CVRanking rank = new CVRanking(this.recordRepo, jdRecord);
-            for (CVRecord cvRecord : cvRecords) {
-                Pair<String, Integer> output = rank.calculateCVScore(cvRecord);
-                System.out.println("File Name: " + output.getKey() + " | Score:  " + output.getValue());
+            List<Pair<String, Integer>> scoreList = getPairs(jdRecord, cvRecords);
+
+            // Print the results in a formatted table
+            System.out.println("----------------------------------------");
+            System.out.printf("%-30s | %-5s\n", "File Name", "Score");
+            System.out.println("----------------------------------------");
+
+            for (Pair<String, Integer> pair : scoreList) {
+                String fileName = pair.getKey();
+                int score = pair.getValue();
+                System.out.printf("%-30s | %-5d\n", fileName, score);
             }
-            System.out.println("\nRanking Complete. Note: This program can make mistakes, it should not be used instead of manual CV review but a tool to help.");
+
+            System.out.println("\nRanking Complete. Note: This program can make mistakes, it should not be used instead of manual CV\nreview but a tool to help.");
         } else {
             System.err.println("No records found");
         }
+    }
 
+    private List<Pair<String, Integer>> getPairs(JobDescriptionRecord jdRecord, List<CVRecord> cvRecords) {
+        CVRanking rank = new CVRanking(this.recordRepo, jdRecord);
+        List<Pair<String, Integer>> scoreList = new ArrayList<>();
+
+        for (CVRecord cvRecord : cvRecords) {
+            Pair<String, Integer> output = rank.calculateCVScore(cvRecord);
+            scoreList.add(output);
+        }
+
+        // Sort by score in descending order
+        scoreList.sort(new Comparator<Pair<String, Integer>>() {
+            @Override
+            public int compare(Pair<String, Integer> p1, Pair<String, Integer> p2) {
+                return p2.getValue().compareTo(p1.getValue()); // Descending order
+            }
+        });
+        return scoreList;
     }
 
 }
